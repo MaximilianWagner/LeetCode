fcece9300ae06897e8c2c650c9eb63a93ecd1bb6
Maximilian Wagner <60820054+MaximilianWagner@users.noreply.github.com>
2022-09-29 13:48:31 +0200

    Time: 6 ms (31.21%), Space: 41.4 MB (16.74%) - LeetHub

---
+++ 507-perfect-number/507-perfect-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+
+        if(num == 1)
+            return false;
+        
+        int sum = 1;
+
+        for (int i = 2; i * i < num; i++) {
+            if (num % i == 0) {
+                sum = sum + i + num / i;
+                if (sum > num)
+                    return false;
+            }
+        }
+
+        return sum == num;
+    }
+}
